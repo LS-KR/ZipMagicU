c15f2fe3203ba210188dac197a1ecf4c906470a3
Elihuso <Elihuso@outlook.com>
2022-12-09 17:05:43 +0800

    添加「binary」选项, 删除「all」选项

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
+using System;
 using System.Text;
 using System.IO;
+using System.Collections.Generic;
 
 using ICSharpCode.SharpZipLib.Zip;
 
 class MainClass
 {
+    public static void ByteToOutput(byte[] source)
+    {
+        for (int i = 0; i < source.Length; i++)
+        {
+            if ((source[i] >= 0x20) && (source[i] < 0x7f))
+                Console.Write(Encoding.ASCII.GetChars(new byte[] { source[i] }));
+            else
+                Console.Write(".");
+        }
+    }
+    public static void ByteToOutputLn(byte[] source)
+    {
+        ByteToOutput(source);
+        Console.WriteLine();
+    }
     public static void Main(string[] args)
     {
-        if (args.Length == 0) return;
-        using (ZipInputStream s = new ZipInputStream(File.OpenRead(args[0])))
+        string fname = null;
+        if (args.Length == 0)
+        {
+            Console.Write("Input the source zip file you want to check.\n> ");
+            fname = Console.ReadLine();
+        }
+        else
+            fname = args[0];
+        using (ZipInputStream s = new ZipInputStream(File.OpenRead(fname)))
         {
 
             ZipEntry theEntry;
@@ -27,10 +50,8 @@ class MainClass
                     rr = "Directory.";
                 Console.WriteLine();
                 Console.WriteLine("-----------------------------------");
-                Console.Write("Is a " + rr + " Show contents?\n[y]es [n]o [a]ll");
+                Console.WriteLine("Is a " + rr + " Show contents?\n[y]es [n]o [b]inary");
                 string inp = Console.ReadLine().ToLower();
-                if ((inp == "a") && (!isallshow))
-                    isallshow = true;
                 if ((inp == "y") || isallshow)
                 {
                     while (true)
@@ -39,6 +60,42 @@ class MainClass
                         if (size > 0)
                         {
                             Console.Write(new ASCIIEncoding().GetString(data, 0, size));
+                            Console.Write("\nExtract (y/n) ?");
+                            if (Console.ReadLine().ToLower() == "y")
+                            {
+                                try
+                                {
+                                    File.WriteAllBytes(rr + "content." + Convert.ToString(i), data);
+                                }
+                                catch { }
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+                if (inp == "b")
+                {
+                    while (true)
+                    {
+                        size = s.Read(data, 0, data.Length);
+                        if (size > 0)
+                        {
+                            List<byte> hexbyte = new List<byte>();
+                            for (int j = 0; j < size; j++)
+                            {
+                                hexbyte.Add(data[j]);
+                                if (j % 16 == 15)
+                                {
+                                    Console.Write(BitConverter.ToString(hexbyte.ToArray()) + "\t");
+                                    ByteToOutputLn(hexbyte.ToArray());
+                                    hexbyte.Clear();
+                                }
+                            }
+                            Console.Write(BitConverter.ToString(hexbyte.ToArray()));
+                            for (int k = hexbyte.ToArray().Length * 3; k <= 48; k++) Console.Write(' '); 
+                            ByteToOutputLn(hexbyte.ToArray());
+                            hexbyte.Clear();
                             Console.Write("\nExtract (y/n) ?");
                             if (Console.ReadLine().ToLower() == "y")
                             {
