3bcea3dabeaa2a56b1e5a98ba48e3a9f61f0df55
Elihuso Quigley <Elihuso@outlook.com>
2024-06-13 21:29:55 +0800

    [S] Reformat code

--- Program.cs
+++ Program.cs
@@ -1,27 +1,26 @@
 using System;
-using System.Text;
-using System.IO;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Text;
 using ICSharpCode.SharpZipLib.Zip;
 
-class MainClass
+internal class MainClass
 {
     public static void ByteToOutput(byte[] source)
     {
-        for (int i = 0; i < source.Length; i++)
-        {
-            if ((source[i] >= 0x20) && (source[i] < 0x7f))
-                Console.Write(Encoding.ASCII.GetChars(new byte[] { source[i] }));
+        for (var i = 0; i < source.Length; i++)
+            if (source[i] >= 0x20 && source[i] < 0x7f)
+                Console.Write(Encoding.ASCII.GetChars(new[] { source[i] }));
             else
                 Console.Write(".");
-        }
     }
+
     public static void ByteToOutputLn(byte[] source)
     {
         ByteToOutput(source);
         Console.WriteLine();
     }
+
     public static void Main(string[] args)
     {
         string fname = null;
@@ -31,9 +30,12 @@ class MainClass
             fname = Console.ReadLine();
         }
         else
+        {
             fname = args[0];
+        }
+
         Console.WriteLine("Contents of the zip file:");
-        using (ZipInputStream s = new ZipInputStream(File.OpenRead(fname)))
+        using (var s = new ZipInputStream(File.OpenRead(fname)))
         {
             ZipEntry list_entry;
             while ((list_entry = s.GetNextEntry()) != null)
@@ -45,16 +47,16 @@ class MainClass
                 Console.WriteLine(list_entry.Name);
             }
         }
-        Console.ResetColor();
-        using (ZipInputStream s = new ZipInputStream(File.OpenRead(fname)))
-        {
 
+        Console.ResetColor();
+        using (var s = new ZipInputStream(File.OpenRead(fname)))
+        {
             ZipEntry theEntry;
-            int size = 2048;
-            byte[] data = new byte[2048];
-            int i = 0;
-            bool isallshow = false;
-            bool isallbinary = false;
+            var size = 2048;
+            var data = new byte[2048];
+            var i = 0;
+            var isallshow = false;
+            var isallbinary = false;
 
             while ((theEntry = s.GetNextEntry()) != null)
             {
@@ -66,41 +68,38 @@ class MainClass
                 Console.WriteLine();
                 Console.WriteLine("-----------------------------------");
                 Console.WriteLine(rr + "\tName: " + theEntry.Name);
-                string inp = "";
-                if ((!isallshow) && (!isallbinary))
+                var inp = "";
+                if (!isallshow && !isallbinary)
                 {
                     Console.WriteLine("Show contents?\n[y]es [n]o [b]inary [a]ll All_b[i]nary");
                     inp = Console.ReadLine().ToLower();
                 }
-                else 
+                else
                 {
                     inp = "";
                     Console.WriteLine();
                 }
+
                 if (inp == "a") isallshow = true;
                 if (inp == "i") isallbinary = true;
-                if ((inp == "y") || isallshow)
-                {
+                if (inp == "y" || isallshow)
                     while (true)
                     {
                         size = s.Read(data, 0, data.Length);
                         if (size > 0)
-                        {
                             Console.Write(new ASCIIEncoding().GetString(data, 0, size));
-                        }
                         else
                             break;
                     }
-                }
+
                 if (inp == "b" || isallbinary)
-                {
                     while (true)
                     {
                         size = s.Read(data, 0, data.Length);
                         if (size > 0)
                         {
-                            List<byte> hexbyte = new List<byte>();
-                            for (int j = 0; j < size; j++)
+                            var hexbyte = new List<byte>();
+                            for (var j = 0; j < size; j++)
                             {
                                 hexbyte.Add(data[j]);
                                 if (j % 16 == 15)
@@ -110,15 +109,20 @@ class MainClass
                                     hexbyte.Clear();
                                 }
                             }
+
                             Console.Write(BitConverter.ToString(hexbyte.ToArray()));
-                            if (hexbyte.Count != 0) for (int k = hexbyte.ToArray().Length * 3; k <= 48; k++) Console.Write(' ');
+                            if (hexbyte.Count != 0)
+                                for (var k = hexbyte.ToArray().Length * 3; k <= 48; k++)
+                                    Console.Write(' ');
                             ByteToOutput(hexbyte.ToArray());
                             hexbyte.Clear();
                         }
                         else
+                        {
                             break;
+                        }
                     }
-                }
+
                 i++;
             }
         }
